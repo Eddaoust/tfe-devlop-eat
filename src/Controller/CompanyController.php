a8a9020e0766de01e38a6ee8dc47b64fa87112c6
Eddaoust <edmond.daoust@gmail.com>
2018-12-19 14:00:56 +0100

    Company Add

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
+use App\Form\CompanyType;
 use App\Repository\CompanyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -14,12 +18,38 @@ class CompanyController extends Controller
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/admin/company", name="company_list")
      */
-    public function companiesList(CompanyRepository $repo)
+    public function listCompany(CompanyRepository $repo)
     {
         $companies = $repo->findAll();
 
         return $this->render('company/company_list.html.twig', [
             'companies' => $companies
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/admin/company/add", name="company_add")
+     */
+    public function addCompany(Request $request, ObjectManager $manager)
+    {
+        $company = new Company();
+
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($company);
+            $manager->flush();
+
+            return $this->redirectToRoute('company_list');
+        }
+
+        return $this->render('company/company_add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -3,26 +3,62 @@
 namespace App\Form;
 
 use App\Entity\Company;
+use App\Entity\CompanyCategory;
+use App\Entity\Country;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CompanyType extends AbstractType
 {
+    /**
+     * @param $label
+     * @param $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('address')
-            ->add('postalCode')
-            ->add('city')
-            ->add('phone')
-            ->add('tvaNum')
-            ->add('registrationNum')
-            ->add('bank')
-            ->add('bankAccount')
-            ->add('country')
-            ->add('companyCategory')
+            ->add('name', TextType::class, $this->getConfiguration('Nom', 'Entrez le nom de la société'))
+            ->add('country', EntityType::class, [
+                'label' => 'Pays',
+                'class' => Country::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Choisissez un pays'
+            ])
+            ->add('companyCategory', EntityType::class, [
+                'label' => 'Raison sociale',
+                'class' => CompanyCategory::class,
+                'choice_label' => 'abbreviation',
+                'placeholder' => 'Choisissez une raison sociale'
+            ])
+            ->add('address', TextType::class, $this->getConfiguration('Adresse', 'Entrez l\'adresse'))
+            ->add('postalCode', TextType::class, $this->getConfiguration('Code Postal', 'Entrez le code postal'))
+            ->add('city', TextType::class, $this->getConfiguration('Ville', 'Entrez la ville'))
+            ->add('phone', TextType::class, $this->getConfiguration('Téléphone', 'Entrez le numéro de téléphone'))
+            ->add('tvaNum', TextType::class, $this->getConfiguration('Numéro de TVA', 'Entrez le numéro de téléphone'))
+            ->add('registrationNum', TextType::class, $this->getConfiguration('Numero d\'enregistrement', 'Pour le Luxembourg'))
+            ->add('bank', TextType::class, $this->getConfiguration('Banque', 'Nom de la banque'))
+            ->add('bankAccount', TextType::class, $this->getConfiguration('Compte banquaire', 'Numero de compte banquaire'))
+            ->add('Ajouter', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
